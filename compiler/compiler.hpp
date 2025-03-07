d5e2be83c122bbb9e77bcc230dbd4ac1d020b787
MrThomasAnderson37@gmail.com <mrthomasanderson37@gmail.com>
2025-03-07 13:22:50 -0800

    tests

--- compiler/compiler.hpp
+++ compiler/compiler.hpp
@@ -3,13 +3,23 @@
 #include "lexer.hpp"
 #include "parser.hpp"
 
+#ifdef _DEBUG
+constexpr bool debug = true;
+#else
+constexpr bool debug = false;
+#endif
+
 inline void compile(std::string& source, std::ostream& os) {
 	Lexer lexer(source);
 	lexer.lex();
-	std::cout << lexer << std::endl;
+	if constexpr (debug) {
+		std::cout << lexer << std::endl;
+	}
 	Parser parser(std::move(lexer.tokens));
 	std::unique_ptr<ASTNode> programNode = parser.parse();
-	std::cout << *programNode << std::endl;
+	if constexpr (debug) {
+		std::cout << *programNode << std::endl;
+	}
 	CodeContext context{ os };
 	programNode->generate(context);
 }
--- compiler/test_compiler.cpp
+++ compiler/test_compiler.cpp
@@ -2,18 +2,80 @@
 #include "simulator.hpp"
 #include "compiler.hpp"
 
-TEST(BasicCompile, TestSuccess) {
+class CompilerTest : public ::testing::Test {
+protected:
+	void SetUp() override {}
+
+	X86Simulator simulator;
 	std::stringstream ss;
+};
+
+TEST_F(CompilerTest, TestSuccess) {
 	std::string source = R"(
 int main() {
 	return 2;
 }
 )";
 	compile(source, ss);
-	X86Simulator simulator;
 	simulator.loadProgram(ss.str());
 	int64_t output = simulator.execute();
 	EXPECT_EQ(2, output);
+}
+
+TEST_F(CompilerTest, TestMissingClosingBrace) {
+	std::string source = R"(
+int main() {
+return 0;
+)";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
+
+TEST_F(CompilerTest, TestMissingOpeningBrace) {
+	std::string source = R"(
+int main()
+return 0;
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
+
+TEST_F(CompilerTest, TestMissingMainFunction) {
+	std::string source = R"(
+int () {
+return 0;
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
+
+TEST_F(CompilerTest, TestMissingReturnStatement) {
+	std::string source = R"(
+int main() {
+	0;
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
+
+TEST_F(CompilerTest, TestInvalidReturnStatement) {
+	std::string source = R"(
+int main() {
+	return ;
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
+
+TEST_F(CompilerTest, TestMissingSemicolon) {
+	std::string source = R"(
+int main() {
+	return 0
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
+
+TEST_F(CompilerTest, TestMissingSpace) {
+	std::string source = R"(
+int main() {
+	return0;
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
 }
 
 int main(int argc, char** argv) {
