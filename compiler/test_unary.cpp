feff038d6c1728fbf87eb147be18bd8ffcc0f3d0
MrThomasAnderson37@gmail.com <mrthomasanderson37@gmail.com>
2025-03-07 16:14:56 -0800

    unary tests

--- compiler/test_unary.cpp
+++ compiler/test_unary.cpp
@@ -10,3 +10,37 @@ int main() {
 	simulator.loadProgram(ss.str());
 	EXPECT_EQ(~12, simulator.execute());
 }
+
+TEST_F(CompilerTest, TestBitwise0) {
+	std::string source = R"(
+int main() {
+	return ~0;
+})";
+	compile(source, ss);
+	simulator.loadProgram(ss.str());
+	EXPECT_EQ(~0, simulator.execute());
+}
+
+TEST_F(CompilerTest, TestMissingConst) {
+	std::string source = R"(
+int main() {
+	return ~;
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
+
+TEST_F(CompilerTest, TestMissingSemicolon2) {
+	std::string source = R"(
+int main() {
+	return ~12
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
+
+TEST_F(CompilerTest, TestWrongOrder) {
+	std::string source = R"(
+int main() {
+	return 12~;
+})";
+	EXPECT_THROW(compile(source, ss), compiler_error);
+}
